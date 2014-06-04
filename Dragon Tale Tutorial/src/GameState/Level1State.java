3f4a74a239d898d33c4c8b40f3a5ed91cf82d4be
Sébastien Saez <monsieursaez@gmail.com>
2014-06-04 18:51:22 +0200

    added changes

--- Dragon Tale Tutorial/src/GameState/Level1State.java
+++ Dragon Tale Tutorial/src/GameState/Level1State.java
@@ -1,6 +1,7 @@
 package GameState;
 
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 import Entity.Player;
 import TileMap.Background;
@@ -29,7 +30,7 @@ public class Level1State extends GameState
 		bg = new Background("/Backgrounds/grassbg1.gif", 0.1);
 		
 		player = new Player(tileMap);
-		
+		player.setPosition(100, 100);
 	}
 	
 	public void update() 
@@ -50,7 +51,67 @@ public class Level1State extends GameState
 		player.draw(g);
 	}
 	
-	public void keyPressed(int k) {}
+	public void keyPressed(int k) 
+	{
+		if(k == KeyEvent.VK_LEFT)
+		{
+			player.setLeft(true);
+		}
+		if(k == KeyEvent.VK_RIGHT)
+		{
+			player.setRight(true);
+		}
+		if(k == KeyEvent.VK_UP)
+		{
+			player.setUp(true);
+		}
+		if(k == KeyEvent.VK_DOWN)
+		{
+			player.setDown(true);
+		}
+		if(k == KeyEvent.VK_W)
+		{
+			player.setJumping(true);
+		}
+		if(k == KeyEvent.VK_E)
+		{
+			player.setGliding(true);
+		}
+		if(k == KeyEvent.VK_R)
+		{
+			player.setScratching();
+		}
+		if(k == KeyEvent.VK_F)
+		{
+			player.setFiring();
+		}
+	}
 	
-	public void keyReleased(int k) {}
+	public void keyReleased(int k) 
+	{
+		if(k == KeyEvent.VK_LEFT)
+		{
+			player.setLeft(false);
+		}
+		if(k == KeyEvent.VK_RIGHT)
+		{
+			player.setRight(false);
+		}
+		if(k == KeyEvent.VK_UP)
+		{
+			player.setUp(false);
+		}
+		if(k == KeyEvent.VK_DOWN)
+		{
+			player.setDown(false);
+		}
+		if(k == KeyEvent.VK_W)
+		{
+			player.setJumping(false);
+		}
+		if(k == KeyEvent.VK_E)
+		{
+			player.setGliding(false);
+		}		
+	}
 }
